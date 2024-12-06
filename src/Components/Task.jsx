19a8cc43f5b3ae2491be1e9f089996d3f0015e69
Vitor Costa Menezes <vitor.menezes@inema.ba.gov.br>
2024-12-06 16:59:32 -0300

    create newtask component

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -2,16 +2,26 @@ import styles from './Task.module.css'
 
 export function Task() {
     return (
-        <div className={styles.info}>
-            <header >
-                <h1>tststststststs</h1>
-            </header>
-            
-            <section className={styles.info}>
+        <>
+            <header className={styles.info}>
 
-            </section>
-        </div>
-        
+                <aside >
+                    Tarefas criadas
+                </aside>
+                
+                <aside>
+                    Concluídas
+                </aside>
+            </header>
+
+            <div className={styles.containerEmpty}>
+                <img src="/src/assets/Clipboard.svg" alt="ícone de prancheta" />
+                <p>
+                    <strong>Você ainda não tem tarefas cadastradas</strong>
+                    Crie tarefas e organize seus itens a fazer
+                </p>
+            </div>
+        </>
       );  
     
 }
