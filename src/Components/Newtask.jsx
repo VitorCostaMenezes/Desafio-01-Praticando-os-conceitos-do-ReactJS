f020591b3d57cf0e6abfa24fa7f2085e1d69b2d4
Vitor Costa Menezes <vitor.menezes@inema.ba.gov.br>
2024-12-12 17:14:00 -0300

    criando states

--- src/Components/Newtask.jsx
+++ src/Components/Newtask.jsx
@@ -1,17 +1,44 @@
 import styles from './NewTask.module.css'
-import { PlusCircle } from '@phosphor-icons/react';
+import { CloudFog, PlusCircle } from '@phosphor-icons/react';
+import { useState } from 'react';
 
 export function NewTask() {
+
+    const [tasks, setTasks] = useState([
+        'Post muito bacana, hein?!'
+      ]);
+
+    console.log(tasks)
+
+    const [newTaskText, setNewTaskText] = useState('');
+
+
+    function handleCrateNewTask() {
+        event.preventDefault()
+    
+        setTasks([...tasks, newTaskText]);
+        setNewTaskText('');
+        console.log(newTaskText)
+      }
+
+    
+
+
     return (
-        <div className={styles.divButton}>
-            <input className={styles.input} type="text"  placeholder="Adicione uma nova tarefa"/>
-            <button className={styles.button} type="button">
-                <span>Criar </span> 
-                <span className={styles.pluscircle}>
-                    <PlusCircle size={16}/> 
-                </span> 
-            </button>
-        </div>
+        <>
+            <form onSubmit={handleCrateNewTask}>
+                <div className={styles.divButton}>
+                    
+                    <input className={styles.input} type="text"  placeholder="Adicione uma nova tarefa" />
+                    <button className={styles.button} type="submit">
+                        <span>Criar </span> 
+                        <span className={styles.pluscircle}>
+                            <PlusCircle size={16}/> 
+                        </span> 
+                    </button>
+                </div>
+            </form>
+        </>
         
       );    
 }
